1eb3d842803882b363c044427f36b256b1e8f4f9
hnguy028 <hieu_nguyen1995@hotmail.com>
2017-11-09 15:20:26 -0500

    stable ui interface template

--- TimeSeriesGraph.py
+++ TimeSeriesGraph.py
@@ -1,24 +1,36 @@
-# import matplotlib.pylab as plb
+#!/usr/bin/env python
+'''
+    TimeSeriesGraph.py : Creates an graphical interface for data streams
+
+    2017Nov09 : Generates line graph panning along with randomly generated data (date vs. rgn point data)
+'''
+
+__author__ = "Hieu Nguyen"
+__created__ = "2017-11-02"
+__version__ = "0.1"
+__status__ = "Development"
+
 import matplotlib.animation as animation
-import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
 import datetime as dt
 import numpy as np
 import pandas as pd
 from dateutil.relativedelta import *
-# import time
 
+'''
+  
+'''
 class TimeSeriesGraph():
-  def __init__(self):
+  def __init__(self, _window_size=25, _step_size=(1, 'min'), _datetime_format='%Y-%m-%d %H:%M'):
     # setup figure
     self.fig = plt.figure()
-    self.fig.autofmt_xdate()
 
     # add subplot
     self.ax = self.fig.add_subplot(1, 1, 1)
 
     # viewing frame size
-    self.viewing_frame = 25
+    self.viewing_frame = _window_size
 
     # default datetime and formats
     self.datetime_format = '%Y-%m-%d %H:%M:%S'
@@ -28,18 +40,20 @@ class TimeSeriesGraph():
 
     self.frame_date = dt.datetime.strptime(str(self.default_datetime), self.datetime_format)
 
-    self.frame_step = 10
-    self.frame_step_measure = 'min'
-    self.x_axis_frequency = str(self.frame_step) + self.frame_step_measure
+    self.x_axis_frequency = str(_step_size[0]) + _step_size[1]
+    self.frame_step = _step_size[0]
+    self.frame_step_measure = _step_size[1]
 
     # init viewing frame limits
-    start = self.current_datetime - dt.timedelta(minutes=self.frame_step)
+    start = self.current_datetime - dt.timedelta(minutes=self.frame_step*self.viewing_frame)
+
     self.ax.set_xlim([start, self.current_datetime])
     self.ax.set_ylim([0, 1])
 
     plt.xticks(rotation=45)
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter(self.datetime_format))
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter(_datetime_format))
     plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+    plt.gcf().autofmt_xdate()
 
     times = self._get_date_axis(str(self.frame_date))
 
@@ -47,44 +61,54 @@ class TimeSeriesGraph():
 
     self.ani = animation.FuncAnimation(self.fig, self._update, frames=self._generator(), interval=400, blit=False)
 
-    plt.gcf().autofmt_xdate()
     plt.show()
+
+  def usage(self):
+    pass
 
   def _generator(self):
     # init
     kwargs = {}
     kwargs["points"] = np.empty(0)
 
-    frame_counter = 1
+    # init generator counter
+    frame_counter = 0
 
     while 1:
-      # increment the x-axis
+      # increment the x-axis frame
       frame_counter += 1
       self.current_datetime = self._increment_datetime(self.current_datetime, auto=True)
 
-      kwargs['n'] = frame_counter
-
-      kwargs['date'] = np.arange(max(0,frame_counter - self.viewing_frame), frame_counter)
-      # kwargs['date'] = np.arange(max(self.default_datetime,self.current_datetime), num)
-      kwargs['datetime'] = self._get_date_axis(self.current_datetime)
-      # kwargs['date'] = np.random.rand(num)
-
+      # generate y axis data
       kwargs["points"] = np.append(kwargs["points"], np.random.rand(1))
-      if len(kwargs["points"]) > self.viewing_frame:
+
+      # clean up points not within viewing frame
+      if len(kwargs["points"]) > self.viewing_frame + 1:
         kwargs["points"] = np.delete(kwargs["points"], 0)
+
+      # generate corresponding x axis data
+      kwargs['datetime'] = self._get_date_axis(self.current_datetime)[-len(kwargs["points"]):]
+
+      # pass along current frame number
+      kwargs['frame_number'] = frame_counter
+
       yield kwargs
 
-  def _get_date_axis(self, datetime):
-    return self.timestamp2datetime(pd.date_range(end=datetime, periods=self.viewing_frame, freq=self.x_axis_frequency))
+  # return a list of datetime objects from ending in the given datetime
+  def _get_date_axis(self, _datetime, _periods=None, _freq=None):
+    return self._timestamp2datetime(pd.date_range(end=_datetime, periods=self.viewing_frame + 1, freq=self.x_axis_frequency))
 
-  def timestamp2datetime(self, timestamp_list):
+  # converts list of timestamp() objects to datetime() objects
+  def _timestamp2datetime(self, timestamp_list):
     return [dt.datetime.strptime(str(timestamp), self.datetime_format) for timestamp in timestamp_list]
 
+  # increment datetime objects by specified times
   def _increment_datetime(self, datetime, auto=False, _seconds=0, _minutes=0, _hours=0, _days=0, _weeks=0, _months=0, _years=0):
-    # to be handled by class variables
+    # to be handled by the TimeSeriesGraph class variables
     if auto:
       return datetime + relativedelta(minutes=self.frame_step)
     else:
+    # increment as specified in the function call
       if _months == 0 and _years == 0:
         # self.frame_date = datetime + dt.timedelta(seconds=_seconds, minutes=_minutes, hours=_hours)
         return datetime + dt.timedelta(seconds=_seconds, minutes=_minutes, hours=_hours)
@@ -92,30 +116,30 @@ class TimeSeriesGraph():
         # self.frame_date = datetime + relativedelta(seconds=_seconds, minutes=_minutes, hours=_hours, days=_days, weeks=_weeks, _months=_months, years=_years)
         return datetime + relativedelta(seconds=_seconds, minutes=_minutes, hours=_hours, days=_days, weeks=_weeks, _months=_months, years=_years)
 
+  # update function called every iteration of the FuncAnimation implementation
   def _update(self, kwargs):
-      n = kwargs['n']
-      xx = kwargs['date']
-      x = kwargs['datetime']
-      y = kwargs["points"]
+    n, x, y = kwargs['frame_number'], kwargs['datetime'], kwargs["points"]
 
-      self.graph.set_xdata(x)
-      self.graph.set_ydata(y)
+    # set axis data
+    # line graph
+    self.ax.plot(x,y)
 
-      self.ax.set_xticklabels(x)
+    # scatter plot
+    # self.ax.plot_date(x,y)
 
-      if n > self.viewing_frame:
-        # usual case when we continuously plot new points
-        # self.ax.set_xlim(n - self.viewing_frame, n)
-        self.ax.set_xlim([self.current_datetime - relativedelta(minutes=self.viewing_frame * self.frame_step), self.current_datetime])
-      else:
-        # when there arent enough points (less than viewing frame)
-        # self.ax.set_xlim(0, self.viewing_frame)
-        self.ax.set_xlim([max(self.default_datetime,
-                              self.current_datetime - dt.timedelta(minutes=self.frame_step*self.viewing_frame)),
-                          self.current_datetime])
-        # self.ax.set_xlim([self.default_datetime, self.default_datetime + relativedelta(minutes=self.viewing_frame * self.frame_step)])
+    # set x axis label for each point
+    self.ax.set_xticks(x)
 
-      return self.graph
+    if n > self.viewing_frame:
+      # usual case when we continuously plot new points
+      self.ax.set_xlim([self.current_datetime - relativedelta(minutes=self.viewing_frame * self.frame_step), self.current_datetime])
+    else:
+      # initial case when there fewer points than self.viewing_frame
+      self.ax.set_xlim([max(self.default_datetime,
+                            self.current_datetime - dt.timedelta(minutes=self.frame_step*self.viewing_frame)),
+                        self.current_datetime])
+
+    return self.graph
 
 if __name__ == '__main__':
   ani = TimeSeriesGraph()
